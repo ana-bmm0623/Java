98245d41b9078c69a4c64f3d06c941e1f7585976
Ana Beatriz Marques Moreira <116691989+ana-bmm0623@users.noreply.github.com>
2023-08-27 22:38:16 +0000

    commit inicial

---
+++ Aula12/aplicacao/Principal.java
@@ -0,0 +1,13 @@
+package Aula12.aplicacao;
+
+
+public class Principal {
+    public static void main(String[] args) {
+    //     Mamifero m = new Mamifero();
+    //     Reptil r= new Reptil();
+    //     Mamifero m = new Mamifero();
+    //     Mamifero m = new Mamifero();
+     }
+}
+
+
